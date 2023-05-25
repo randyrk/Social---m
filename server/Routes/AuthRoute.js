fbb589c4598fa40fe9368876ca88e4d26199a243
randyrk <santhoshvs893@gmail.com>
2023-05-25 17:58:06 +0530

    social

--- server/Routes/AuthRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerUser } from "../Controllers/AuthController.js";
-
-
-const router = express.Router()
-
-router.post('/register',registerUser)
-
-export default router
